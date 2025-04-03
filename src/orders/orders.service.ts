ec8a8871d81da91fd15b425f34760a766de5413b
castillo <fabianboc960@hotmail.com>
2025-04-03 17:39:13 -0500

    feat: :sparkles: Integrar Prisma y crear esquema de base de datos
    Se añadió la dependencia '@prisma/client' en package.json y package-lock.json. Se creó el archivo de esquema 'schema.prisma' para definir el modelo de datos de órdenes y se generaron las migraciones correspondientes para la base de datos PostgreSQL. Además, se actualizó el servicio de órdenes para extender PrismaClient y registrar la conexión a la base de datos.

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger(OrdersService.name);
+
+  onModuleInit() {
+    this.logger.log(`Database Connected`)
+  }
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
