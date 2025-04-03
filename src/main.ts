ae4d057e83d54f8fe43eae1082e4b1af158f251d
castillo <fabianboc960@hotmail.com>
2025-04-03 12:27:08 -0500

    feat: :sparkles: Integrar microservicios en la aplicación
    Se añadió la dependencia '@nestjs/microservices' en package.json. Además, se actualizó main.ts para crear un microservicio utilizando el transporte TCP y se ajustó el logger para reflejar el nuevo contexto de microservicio.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const logger = new Logger('Main - Orders');
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.PORT);
+
+  const logger = new Logger('OrderMS - Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT
+      }
+    },
+  );
+
+  await app.listen();
   logger.log(`Orders Microservice Running on port ${ envs.PORT }`);
 }
 bootstrap();
