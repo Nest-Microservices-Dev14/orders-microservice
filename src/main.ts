733537bb43b621a5140891f8137538771f564109
castillo <fabianboc960@hotmail.com>
2025-04-03 12:19:23 -0500

    feat: :sparkles: Agregar soporte para variables de entorno y validación
    Se añadió un archivo de plantilla .env, se incorporaron las dependencias 'dotenv' y 'joi' en package.json y package-lock.json, y se implementó la carga y validación de variables de entorno en el nuevo archivo src/config/envs.ts. Además, se actualizó main.ts para utilizar la variable de entorno PORT.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main - Orders');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.PORT);
+  logger.log(`Orders Microservice Running on port ${ envs.PORT }`);
 }
 bootstrap();
