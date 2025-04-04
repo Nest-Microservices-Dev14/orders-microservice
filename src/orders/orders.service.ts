2257ddeb9eedb316a0e646017e3a8d35d35f626d
castillo <fabianboc960@hotmail.com>
2025-04-04 11:44:28 -0500

    feat: :sparkles: Añadir validación y DTO para órdenes
    Se incorporaron las dependencias 'class-transformer' y 'class-validator' en package.json y package-lock.json. Se implementó el DTO 'CreateOrderDto' con validaciones para los campos 'totalAmount', 'totalItems', 'status' y 'paid'. Además, se actualizó el controlador y servicio de órdenes para manejar la validación y se creó un nuevo archivo de enumeración para los estados de las órdenes.

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,24 +1,39 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
 
   private readonly logger = new Logger(OrdersService.name);
 
-  onModuleInit() {
+  async onModuleInit() {
+    await this.$connect();
     this.logger.log(`Database Connected`)
   }
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+
+  async create(createOrderDto: CreateOrderDto) {
+    return await this.order.create({
+      data: createOrderDto
+    });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll() {
+    return await this.order.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string) {
+    
+    const order = await this.order.findFirst({
+      where: { id }
+    });
+
+    if( !order ) throw new RpcException({
+      status: HttpStatus.NOT_FOUND,
+      message: `Order with id ${ id } not found`
+    });
+
+    return order;
   }
 }
