52bd0573e0d1e0af916efe5b89d55a0dd6c0128a
castillo <fabianboc960@hotmail.com>
2025-04-04 16:24:54 -0500

    feat: :sparkles: Añadir paginación y manejo de excepciones en órdenes
    Se creó el DTO 'PaginationDto' para la paginación de resultados y se implementó 'OrderPaginationDto' para incluir el estado de las órdenes. Además, se añadió un filtro de excepciones personalizado 'RpcCustomExceptionFilter' para manejar errores en las respuestas de microservicios. Se actualizaron el controlador y servicio de órdenes para soportar la paginación y el cambio de estado de las órdenes.

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,6 +2,9 @@ import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
 import { RpcException } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
+import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -19,8 +22,28 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  async findAll() {
-    return await this.order.findMany();
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+
+    const { page, limit, status } = orderPaginationDto;
+    
+    const totalPages = await this.order.count({
+      where: { status }
+    });
+    const currentPage = page;
+    const perPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.order.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { status }
+      }),
+      meta:{
+        total: totalPages,
+        page: currentPage,
+        lastPage: perPage,
+      }
+    }
   }
 
   async findOne(id: string) {
@@ -35,5 +58,20 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     });
 
     return order;
+  }
+
+  async changeStatus(changeOrderStatusDto: ChangeOrderStatusDto){
+
+    const { id, status } = changeOrderStatusDto;
+
+    const order = await this.findOne( id );
+
+    if( status === order.status ) return order;
+
+    return this.order.update({
+      where: { id },
+      data: { status }
+    });
+
   }
 }
